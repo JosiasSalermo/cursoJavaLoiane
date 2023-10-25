b29796a6ef236d606478dec91ffbb3059197794d
Josias <josiassalermojunior>
2023-10-25 06:10:52 -0300

    aula 10

---
+++ javaBasico/src/aula10/Variaveis.java
@@ -0,0 +1,29 @@
+package aula10;
+
+public class Variaveis {
+    public static void main(String[] args) {
+
+        // Convenção Java
+        int idade = 20;
+        String nome = "Loiane";
+        String nomeDoMeuCachorro = "totó";
+        String ano2014 = "2014";
+
+        // Aceito, mas não utilizado
+        int _idade;
+        int $idade;
+
+        // Não é convenção Java
+        String nome_do_meu_cachorro;
+        String NomeDoMeuCachorro;
+
+        idade = 25;
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+
+        // Má prática
+        int a = 10;
+        String b = "Loiane";
+    }
+}
