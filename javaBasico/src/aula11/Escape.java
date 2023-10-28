e7a51d550f5f4d85056d790ab961c7ae28f88065
Josias <josiassalermojunior>
2023-10-28 07:20:11 -0300

    aula 11

---
+++ javaBasico/src/aula11/Escape.java
@@ -0,0 +1,9 @@
+package aula11;
+
+public class Escape {
+    public static void main(String[] args) {
+
+        // "hello, World!"
+        System.out.print("\"Hello, World!\"\n\r");
+    }
+}
---
+++ javaBasico/src/aula11/PontoFlutuante.java
@@ -0,0 +1,11 @@
+package aula11;
+
+public class PontoFlutuante {
+    public static void main(String[] args) {
+        double valorPassagem = 2.9;
+        float valorTomate = 3.95F;
+
+        System.out.println("Valor da passagem = " + valorPassagem);
+        System.out.println("Valor do tomate = " + valorTomate);
+    }
+}
